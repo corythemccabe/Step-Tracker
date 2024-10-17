be630b146cc396e5591e07cdcc4ab1f4d02deed8
Cory McCabe <83873828+corythemccabe@users.noreply.github.com>
2024-10-17 10:46:59 -0500

    Added Xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Cory McCabe on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
