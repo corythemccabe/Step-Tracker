0222e71e8dfeed91780be2bea6034990d3287abd
Cory McCabe <83873828+corythemccabe@users.noreply.github.com>
2024-10-26 12:39:36 -0500

    Added sample data

--- Step Tracker/HealthKitManager.swift
+++ Step Tracker/HealthKitManager.swift
@@ -18,6 +18,25 @@ import Observation
     init() {
         self.healthStore = HKHealthStore()
     }
+    
+    func  addSimulotarData() async {
+        var samples: [HKQuantitySample] = []
+       
+        for i in 0..<28 {
+            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
+            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
+            let endDate = Calendar.current.date(byAdding: .day, value: -i + 1, to: .now)!
+            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
+         
+            let heartRateQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 60...120))
+            let heartRateSample = HKQuantitySample(type: HKQuantityType(.heartRate), quantity: heartRateQuantity, start: startDate, end: endDate)
+            
+            samples.append(stepSample)
+            samples.append(heartRateSample)
+        }
+        
+        try! await healthStore.save(samples)
+    }
 }
 
 
